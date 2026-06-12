2418adb3c02936471f49d61993801ff7ef52066d
NuIIskill <nullskilll@gmail.com>
2026-06-12 12:36:39 +0200

    Apply updated gitignore

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_OpenPDFStudio_FILE /home/nullskill_0_0/Projects/OpenPDF-Studio/build/OpenPDFStudio)
-set(__QT_DEPLOY_TARGET_OpenPDFStudio_TYPE EXECUTABLE)
--- build/CMakeFiles/OpenPDFStudio.dir/OpenPDFStudio_autogen/3YJK5W5UP7/qrc_openpdf-studio.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/OpenPDFStudio.dir/OpenPDFStudio_autogen/3YJK5W5UP7/qrc_openpdf-studio.cpp.o: \
- /home/nullskill_0_0/Projects/OpenPDF-Studio/build/OpenPDFStudio_autogen/3YJK5W5UP7/qrc_openpdf-studio.cpp \
- /usr/include/stdc-predef.h
